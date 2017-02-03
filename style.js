30dffacdbebfbe031d3aa3b52b6cebc26ab1b5a7
ahsirt828 <trishaannesalcedo@gmail.com>
2017-02-03 15:01:32 +0800

    add css,js and modified html

---
+++ style.js
@@ -0,0 +1,44 @@
+(function($) {
+  $.fn.slideshow = function(options) {
+    options = $.extend({
+      'timeout': 3000,
+      'speed': 100 
+    }, options);
+    // We loop through the selected elements, in case the slideshow was called on more than one element e.g. `$('.foo, .bar').slideShow();`
+    return this.each(function() {
+      // Inside the setInterval() block, `this` references the window object instead of the slideshow container element, so we store it inside a var
+      var $elem = $(this);
+      $elem.children().eq(0).appendTo($elem).show();
+      // Iterate through the slides
+      setInterval(function() {
+        $elem.children().eq(0)
+          // Hide the current slide and append it to the end of the image stack
+          .hide().appendTo($elem) // As of jQuery 1.3.2, .appendTo() returns the inserted element
+          // Fade in the next slide
+          .fadeIn(options.speed)
+      }, options.timeout);
+    });
+  };
+}(jQuery));
+
+// Name the slider
+
+$(function() {
+  $('.slides').slideshow({
+    timeout: 5000,
+    speed: 500
+  });
+});
+
+jQuery(document).ready(function($) {
+  $(window).scroll(function() {
+    var scrollPos = $(window).scrollTop(),
+        navbar = $('.navbar-default');
+
+    if (scrollPos > 20) {
+      navbar.addClass('change-color');
+    } else {
+      navbar.removeClass('change-color');
+    }
+  });
+});
